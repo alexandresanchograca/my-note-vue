0f590f192459a9fa206c0aac34f55bc45041714e
alexandresanchograca <alexandresanchograca@gmail.com>
2024-05-28 19:20:45 +0100

    fixed layout

--- src/views/SharedView.vue
+++ src/views/SharedView.vue
@@ -5,8 +5,19 @@
         <li><i class="fa-solid fa-user"></i> teste@maisdasdasdasdasdaad.pt</li>
         <li><i class="fa-solid fa-user"></i> teste@mail.pt</li>
         <li><i class="fa-solid fa-user"></i> teste@mail.pt</li>
+        <li><i class="fa-solid fa-user"></i> ...</li>
+      </ul>
+      <div class="note-title">
+        <h3>My simple note titlessss dasdasdasd asdasdasda</h3>
+      </div>
+      <button>View</button>
+    </div>
+    <div class="note">
+      <ul>
+        <li><i class="fa-solid fa-user"></i> teste@maisdasdasdasdasdaad.pt</li>
         <li><i class="fa-solid fa-user"></i> teste@mail.pt</li>
         <li><i class="fa-solid fa-user"></i> teste@mail.pt</li>
+        <li><i class="fa-solid fa-user"></i> ...</li>
       </ul>
       <div class="note-title">
         <h3>My simple note titlessss dasdasdasd asdasdasda</h3>
@@ -22,18 +33,20 @@
 #shared-notes {
   display: flex;
   flex-direction: column;
-  padding: 16px 10px;
   margin: 5px;
-  background: var(--secondary);
-  border-radius: 8px;
+  gap: 20px;
 }
 .note {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  box-sizing: border-box;
+  border-radius: 8px;
+  padding: 16px 10px;
   gap: 2.8rem;
   height: 140px;
   overflow: hidden;
+  background: var(--secondary);
 }
 
 .note-title {
@@ -46,7 +59,6 @@ h3 {
 }
 
 ul {
-  align-self: flex-start;
   list-style-type: none;
   padding: 0;
   line-height: 1.6rem;
@@ -57,18 +69,18 @@ ul {
 }
 
 li {
+  font-size: 0.85rem;
   text-wrap: nowrap;
 }
 
 .fa-user {
-  font-size: 0.85rem;
   color: #252222;
 }
 
 button {
   margin: 20px;
   width: 160px;
-  height: 75%;
+  height: 80%;
   background-color: #474040;
   flex-shrink: 0;
 }
