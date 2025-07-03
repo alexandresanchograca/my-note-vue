38e0d745b6689376a57cced68f24435b08b3f90c
alexandresanchograca <hatumgit@gmail.com>
2025-07-04 00:10:51 +0100

    added StorageView and StorageView route

---
+++ src/views/StorageView.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="handleUpload">Upload new file</button>
+  <div v-for="item in storageItems" :key="item.id" class="storage-item">
+    {{ item.id }}
+  </div>
+  <div class="storage-view-component">Foobar</div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const storageItems = ref([{ id: "kappa" }]);
+
+function handleUpload() {
+  console.log("Uploading file...");
+}
+</script>
+
+<style></style>
